f64f833caf877bcc4821af18d06bc2ef41ad9e62
Arshat Talap <t_arshat@mail.ru>
2023-05-25 11:34:23 +0600

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Аршат Талап on 25.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
